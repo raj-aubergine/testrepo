05ba4e4174b72b35c6139fc157834d59db8a686f
rajaubergine <raj.v@auberginesolutions.com>
2022-01-05 17:53:05 +0530

    water

--- test.py
+++ test.py
@@ -2,4 +2,6 @@ hungry=input("do you wanna eat?")
 if hungry == "yes":
     print("eat pizza")
 else:
-    print("do your work")
+    thirsty=input("are you thirsty")
+    if thirsty == "yes":
+        print("drink water")
